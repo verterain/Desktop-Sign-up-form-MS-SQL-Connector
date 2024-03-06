0e9152aa5db499b0b3c3339223b6d3ffa2ffa7ac
Michał Jędrszczak <122973948+verterain@users.noreply.github.com>
2024-03-06 11:48:19 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import pyodbc
 
-server = '{your_server_name}'
+server = 'your_server_name'
 database = 'master'  
 
 conn_str = f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes;'
