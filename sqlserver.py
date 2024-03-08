f85f6dd7d52808c74b9047b80606f3ef69e16de8
Michał Jędrszczak <122973948+verterain@users.noreply.github.com>
2024-03-08 14:55:01 +0100

    Update sqlserver.py

--- sqlserver.py
+++ sqlserver.py
@@ -1,10 +1,6 @@
 import pyodbc
 
-<<<<<<< HEAD
-server = 'DESKTOP-F9RFMJE\\SQLEXPRESS'
-=======
 server = 'your_server_name'
->>>>>>> ed61fcc6a2fdc37d1179a0f69a4b03091ef682b3
 database = 'usernames'  
 
 conn_str = f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes;'
